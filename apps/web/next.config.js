d912e509b1d1e785e37ff52c91f132b68a335e31
Vladimir Ulyanov <vulyanov@spbfiller.com>
2023-07-30 18:48:58 +0300

    fix(initial): fix setting for turbopack

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,10 +3,11 @@ const nextConfig = {
   transpilePackages: ["@d4comm/ui"],
   compress: false,
   experimental: {
-    typedRoutes: true,
     mdxRs: true,
-    webVitalsAttribution: ["FCP", "LCP", "CLS", "FID", "TTFB", "INP"],
-    instrumentationHook: true,
+    /** Disabled until support in turbopack */
+    // typedRoutes: true,
+    // webVitalsAttribution: ["FCP", "LCP", "CLS", "FID", "TTFB", "INP"],
+    // instrumentationHook: true,
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
